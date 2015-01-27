e8971e805f6ba0b2734e39cc01e827803d5b0de5
Tom Hinkle <ratedungeon@gmail.com>
2015-01-27 11:13:02 -0600

    Beginning the groundwork for semester views.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from os.path import abspath, dirname, join
+from datetime import datetime
 
 _basedir = abspath(dirname(__file__))
 
@@ -9,6 +10,8 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///" + join(_basedir, 'app.db')
 # These configurations are used for my own purposes
 ALERT_CATEGORIES = {"SUCCESS": "success", "INFO": "info", "ERROR": "danger"}
 SITE_VERSION = "ALPHA"
+CUR_YEAR = datetime.now().year
+SEMESTERS = {'Fall': (8, 9, 10, 11, 12), 'Spring': (1, 2, 3, 4, 5)}
 STATUSES = ("Unclaimed", "Repairing", "Waiting", "Complete")
 
 
